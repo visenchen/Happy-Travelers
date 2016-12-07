be6236b5b2fe910642a85d835464d43377f878b1
Visen <visenchen@VisendeMacBook-Air.local>
2016-12-08 00:56:40 +0900

    初始化项目

---
+++ Happy Travelers/Happy Travelers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happy Travelers
+//
+//  Created by Visen on 2016/12/8.
+//  Copyright © 2016年 Visen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
